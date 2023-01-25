1bb8a76bcf396f7dd1b71db5bd946dcf5d618631
Ben <danquahwhite@gmail.com>
2023-01-25 15:21:10 +0000

    Completed sliders section

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -6,6 +6,7 @@ export function Index() {
     <main>
       <Carousel
         className="-z-30"
+        dotPosition="right"
         autoplay
         autoplaySpeed={5000}
       >
@@ -102,6 +103,31 @@ export function Index() {
               </div>
 
               <div className="mt-4 space-x-4">
+                {/*<button className="btn btn-accent btn-md">Apply Now</button>*/}
+                <button className="btn btn-info btn-md">Contact Us</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-cover bg-right-top bg-gray-300 slide5 h-120 lg:h-100">
+          <div className="container mx-auto py-16 px-4 lg:px-10">
+            <div className="bg-gray-800 p-8 lg:w-2/5 bg-opacity-75 rounded-lg">
+              <div className="uppercase text-accent text-lg mb-2">
+                achieve your business goals
+              </div>
+              <div className="text-white text-2xl">
+                Help your business reach its full potential through our
+              </div>
+              <div className="text-4xl text-orange-400 font-black uppercase drop-shadow my-4">
+                General Business Consultancy Service
+              </div>
+
+              <div className="text-white mt-2">
+                We understand that running a business can be challenging, which is why we offer a wide range of services to help you navigate the complexities of the business world.
+              </div>
+
+              <div className="mt-4 hidden space-x-4">
                 {/*<button className="btn btn-accent btn-md">Apply Now</button>*/}
                 <button className="btn btn-info btn-md">Contact Us</button>
               </div>
