010f82a8cb86f6103ff0ab3da243fca1ffa6d53f
Ben <danquahwhite@gmail.com>
2023-01-27 17:24:05 +0000

    update

--- apps/website/pages/index.tsx
+++ apps/website/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Carousel } from 'antd';
 import { Collapse } from 'antd';
+import {FiMail, FiMapPin, FiPhone} from 'react-icons/fi';
+import {RiCustomerService2Line} from "react-icons/ri";
 
 
 
@@ -189,8 +191,38 @@ export function Index() {
       </div>
 
       <div className="px-16 relative z-40">
-        <div className="bg-accent rounded-2xl -mb-10 py-12" style={{backgroundImage: `url("/images/shape-100.png")`}}>
+        <div className="bg-white drop-shadow-2xl rounded-2xl -mb-10 py-12" style={{backgroundImage: `url("/images/shape-100.png")`}}>
             <div className="px-12 grid grid-cols-2 gap-16">
+              <div className="border-l border-neutral border-l-2 pl-16">
+                <div className="text-4xl text-error mb-4"><RiCustomerService2Line/></div>
+                <div className="text-4xl text-neutral">Convinced yet? Let&apos;s make <br/>something great together.
+                </div>
+                <div className="mt-4">
+                  <div className="grid grid-cols-12 gap-2">
+                    <div><FiMapPin className="text-secondary"/></div>
+                    <div>
+                      <div className="font-bold">Address</div>
+                      <div>Adjacent Pentecost, Adenta - Accra, Ghana</div>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-12 gap-2">
+                    <div><FiPhone/></div>
+                    <div>
+                      <div className="font-bold">Phone</div>
+                      <div>+233(0)501064110 / 0302549659</div>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-12 gap-2">
+                    <div><FiMail/></div>
+                    <div>
+                      <div className="font-bold">Email</div>
+                      <div>info@fixlinkconsultgh.com</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
               <div className="p-8 bg-neutral rounded-lg">
                 <div className="text-3xl text-white font-bold">Have any questions?</div>
@@ -221,60 +253,6 @@ export function Index() {
                   <button className="btn btn-secondary">Submit</button>
                 </div>
               </div>
-              <div className="text-2xl text-primary font-bold border-l border-neutral border-l-2 pl-16">
-                <div>Frequently Asked Question (FAQs)</div>
-                <div className="mt-4">
-                  <Collapse accordion bordered={false} ghost activeKey={1}>
-                    <Panel header="Why study abroad?" key="1">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        Its good to learn from other cultures and to learn in a whole new cosmopolitan environment is awesome.
-                        It helps in acquisition of a modern way of doing things and also provides better facilities to learn with.
-                      </div>
-                    </Panel>
-                    <Panel header="How many days does it take to get birth cert done?" key="2">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        We typically get your birth certificates done with 2 business weeks.
-                        That is 14 days from commencement.
-                      </div>
-                    </Panel>
-                    <Panel header="How do I get started with my application to study in abroad?" key="3">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        <ol className="list-decimal list-inside">
-                          <li>Choose your programme you intend to study</li>
-                          <li>Provide us with your preferred country if any</li>
-                          <li>Provide us with your contact details</li>
-                          <li>Provide your current level of education</li>
-                        </ol>
-                      </div>
-                    </Panel>
-                    <Panel header="This is my first time travelling, what do I need to get my Visa?" key="4">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        <div>
-                          You will need to first have your passport. If you do not have it already, please contact.
-                          Then let us know where you want to go, we handle the rest for you.
-                        </div>
-                      </div>
-                    </Panel>
-                    <Panel header="Are there Scholarship Programmes Available?" key="5">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        <div>
-                          YES! Scholarships are available for most countries
-                        </div>
-                      </div>
-                    </Panel>
-
-                    <Panel header="  What's Next After Gaining My Acceptance Letter?" key="6">
-                      <div className="bg-neutral rounded-lg text-gray-200 px-3 py-2">
-                        <div>
-                          At this stage, we guide you on the necessary documents that the embassy or consulate require from yourself, parents or guardians to decide whether to give you the visa or not.
-                          Our job is to provide you with the vital tools which would enable you to get the visa.
-                        </div>
-                      </div>
-                    </Panel>
-                  </Collapse>
-                </div>
-              </div>
-
             </div>
         </div>
       </div>
